6838332f19184a6b7b337e60723797561ae0b9af
Guoliang Li <guoliang.me@gmail.com>
2016-02-21 16:50:34 +0800

    updated url

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='rabbit-backup',
-      version='0.0.2',
+      version='0.0.3',
       description='backup local files to dropbox. rabbit_backup_to_dropbox will remove local/remote files after verified based on setting. ',
       author='Guoliang Li',
       author_email='dev@liguoliang.com',
-      url='http://rabbitsrc.com/backup',
+      url='http://guoliang-dev.github.io/rabbit-backup',
       packages=['rabbit_backup'],
       scripts=['scripts/rabbit-backup.py'],
       license='Apache Software License',
