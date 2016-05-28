4949897a20460abdc6ac65d46768690a9b76d4bc
Guoliang Li <guoliang.me@gmail.com>
2016-05-28 15:30:44 +0800

    added logging.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rabbit-backup',
-      version='0.0.5',
+      version='0.0.6',
       description='backup local files to dropbox. rabbit_backup_to_dropbox will remove local/remote files after verified based on setting. ',
       author='Guoliang Li',
       author_email='dev@liguoliang.com',
@@ -9,5 +9,5 @@ setup(name='rabbit-backup',
       packages=['rabbit_backup'],
       scripts=['scripts/rabbit-backup.py'],
       license='Apache Software License',
-      install_requires=['dropbox', 'python-dateutil']
+      install_requires=['dropbox==4.0', 'python-dateutil']
      )
